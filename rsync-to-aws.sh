70b53f9d22b67abedf70b5969c42a26896db5673
Jillian Rowe <jillian.e.rowe@gmail.com>
2020-09-16 12:03:07 +0300

    building dev instance

--- rsync-to-aws.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd ..
-rsync -avz \
-    -e "ssh -i genome-portal/.ssh/nrl-genomics-lims-keypair.pem" \
-    genome-portal \
-    ec2-user@ec2-100-25-2-88.compute-1.amazonaws.com:/home/ec2-user/efs/
